571ee79084f7436dc85788ed7fb8b0f3163b0e97
Bernabeus <bernabeus123@gmail.com>
2022-02-10 11:13:49 -0500

    Primer commit: Creacion del proyecto

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Test CHASIDE</title>
+        <meta name="description" content="Test de orientación vocacional CHASIDE" />
+        <link rel="icon" href="/logo.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        
+      </main>
+
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
